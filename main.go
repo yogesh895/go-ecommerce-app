551561dd5b903071e789c63b5ad5e9ffe06773a8
Yogesh <yogesh.sangtani.5@gmail.com>
2024-12-24 22:06:06 +0000

    Added Opentelemetry instrumentation

--- main.go
+++ main.go
@@ -1,18 +1,93 @@
 package main
 
 import (
-	"go-ecommerce-app/routes"
+	"context"
+	"log"
+	"os"
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	"go-ecommerce-app/routes"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"google.golang.org/grpc/credentials"
 )
 
+var (
+	serviceName  = os.Getenv("SERVICE_NAME")
+	collectorURL = os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	insecure     = os.Getenv("INSECURE_MODE")
+)
+
+// initTracer initializes OpenTelemetry tracer and returns a shutdown function.
+func initTracer() func(context.Context) error {
+	var secureOption otlptracegrpc.Option
+
+	// Configure secure or insecure connection
+	if strings.ToLower(insecure) == "false" || insecure == "0" || strings.ToLower(insecure) == "f" {
+		secureOption = otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+	} else {
+		secureOption = otlptracegrpc.WithInsecure()
+	}
+
+	// Create exporter
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(collectorURL),
+		),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create exporter: %v", err)
+	}
+
+	// Set up resource attributes
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", serviceName),
+			attribute.String("library.language", "go"),
+		),
+	)
+	if err != nil {
+		log.Fatalf("Could not set resources: %v", err)
+	}
+
+	// Configure the global tracer provider
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.AlwaysSample()),
+			sdktrace.WithBatcher(exporter),
+			sdktrace.WithResource(resources),
+		),
+	)
+
+	return exporter.Shutdown
+}
+
 func main() {
+	// Initialize tracer and ensure cleanup is called on exit
+	cleanup := initTracer()
+	defer cleanup(context.Background())
+
 	// Create a new Gin router
 	r := gin.Default()
+
+	// Add OpenTelemetry middleware
+	r.Use(otelgin.Middleware(serviceName))
 
 	// Setup routes
 	routes.SetupRoutes(r)
 
 	// Start the server on port 8080
-	r.Run(":8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
 
